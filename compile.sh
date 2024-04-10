a055c0f0894c3df66623c8055b690265fa50e2ac
waurttttt@gmail.com <igor@ArchIgor.netis>
2024-04-10 16:11:02 +0300

    Sorted files

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -c main.cpp mainMenu.cpp button.cpp cutscenes.cpp
+g++ -c main.cpp Menu/mainMenu.cpp Menu/button.cpp Menu/cutscenes.cpp
 g++ main.o mainMenu.o button.o cutscenes.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
