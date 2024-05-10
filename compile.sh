7b76e3f641c5de1bf3ddb40bb32f2fd4e1b47659
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2024-05-10 20:47:55 +0300

    Deleted unwanted files

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-g++ -c main.cpp Menu/mainMenu.cpp Menu/button.cpp Menu/cutscenes.cpp Entities/entity.cpp Entities/aliveEntity.cpp Entities/player.cpp
-g++ main.o mainMenu.o button.o cutscenes.o entity.o aliveEntity.o player.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
